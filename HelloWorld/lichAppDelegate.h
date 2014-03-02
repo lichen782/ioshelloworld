6cf98ce0a8ee10138191a85f68c02c8a50a4aafe
lich <lichen782@gmail.com>
2014-03-02 09:38:33 +0800

    IOS: First commit "Hello World"

---
+++ HelloWorld/lichAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  lichAppDelegate.h
+//  HelloWorld
+//
+//  Created by 李晨 on 14-3-2.
+//  Copyright (c) 2014年 李晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lichAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by 李晨 on 14-3-2.
+//  Copyright (c) 2014年 李晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "lichAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([lichAppDelegate class]));
+    }
+}
